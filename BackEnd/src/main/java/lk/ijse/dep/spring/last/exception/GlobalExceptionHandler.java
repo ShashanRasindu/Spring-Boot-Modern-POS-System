72b0c4f9e295646ac723fcbc26429821ed3a1c74
shashan <shashanrasindu@gmail.com>
2019-06-21 14:06:54 +0530

    spring boot complet project

---
+++ BackEnd/src/main/java/lk/ijse/dep/spring/last/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep.spring.last.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String globalExceptionHandlerMethod(Exception ex){
+        return ex.getMessage();
+    }
+
+}
